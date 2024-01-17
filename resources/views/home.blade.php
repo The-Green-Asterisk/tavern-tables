cbdde5461485595c7eb6623cfa367fe78936764d
Steve Beaudry <live.remix@gmail.com>
2024-01-17 08:40:24 -0500

    modal styles and home section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layout')
+@section('content')
 <x-nav />
 
 Tavern Tables is an app that allows you to sign up for and schedule tabletop games. It uses the concept of a "tavern owner" that can create multiple tables, assign game masters to them and invite players to join. It's perfect for facilities or organizations that want to host multiple games at once, but it's also great for singular games that want to keep track of their players and schedules.
@@ -7,3 +8,4 @@ Tavern Tables is an app that allows you to sign up for and schedule tabletop gam
     <button id="tavern-owner-join">Join as Tavern Owner</button>
     <button id="player-join">Join as Player</button>
 </div>
+@endsection
